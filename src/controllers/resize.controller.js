11540aae2f55846b358248d7024d2b2fb563e34d
Jitesh <jitesh.srivastava@gmail.com>
2022-05-07 23:03:00 +0530

    jest setup - arrange awaits

--- src/controllers/resize.controller.js
+++ src/controllers/resize.controller.js
@@ -5,55 +5,55 @@ import { INVALID_REQUEST, INVALID_IMAGE } from '../constants/constants.js';
 import axios from 'axios';
 
 function isBuffer(buffer) {
-    return Buffer.isBuffer(buffer);
+	return Buffer.isBuffer(buffer);
 }
 
 const resize = async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) 
-            return res.status(422).json({ message: INVALID_REQUEST, errors: errors.array() });
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) 
+		return res.status(422).json({ message: INVALID_REQUEST, errors: errors.array() });
     
-        try {
-            const width = req.query.width;
-            const height = req.query.height;
-            const url = decodeURI(req.query.url);
-            const imageBuffer = await getImage(url);
-            const validImage = isBuffer(imageBuffer);
-            const resizedImage = await ResizeService.resize(imageBuffer,parseInt(width), parseInt(height));
-            const isValidResizedImage = isBuffer(resizedImage);
-            if(isValidResizedImage) {
-                res.set("Content-Type", "image/png");
-                res.status(StatusCodes.ACCEPTED).send(resizedImage);
-            }
-            else {
-                console.log(`Image Valid - ${validImage}, Resized Image Valid - ${isValidResizedImage}`);
-                res.status(StatusCodes.BAD_REQUEST).send({ message : INVALID_IMAGE });
-            }
-    } catch (error) {
-            console.log(error);
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send( { error: 'Error while processing request', message : error.message });
-    }
-}
+	try {
+		const width = req.query.width;
+		const height = req.query.height;
+		const url = decodeURI(req.query.url);
+		const imageBuffer = await getImage(url);
+		const validImage = isBuffer(imageBuffer);
+		const resizedImage = await ResizeService.resize(imageBuffer,parseInt(width), parseInt(height));
+		const isValidResizedImage = isBuffer(resizedImage);
+		if(isValidResizedImage) {
+			res.set('Content-Type', 'image/png');
+			res.status(StatusCodes.ACCEPTED).send(resizedImage);
+		}
+		else {
+			console.log(`Image Valid - ${validImage}, Resized Image Valid - ${isValidResizedImage}`);
+			res.status(StatusCodes.BAD_REQUEST).send({ message : INVALID_IMAGE });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).send( { error: 'Error while processing request', message : error.message });
+	}
+};
 
 async function convertStreamToBuffer(stream) {
-    return new Promise((resolve, reject) => {
-        const buffer = [];
-        stream.on('data', (chunk) => {
-            buffer.push(chunk);
-        });
-        stream.on('end', () => resolve(Buffer.concat(buffer)));
-        stream.on('error', (err) => reject(err));
-    });
+	return new Promise((resolve, reject) => {
+		const buffer = [];
+		stream.on('data', (chunk) => {
+			buffer.push(chunk);
+		});
+		stream.on('end', () => resolve(Buffer.concat(buffer)));
+		stream.on('error', (err) => reject(err));
+	});
 }
 
 const getImage = async (url) => {
-    try {
-        const response = await axios.get(url, {responseType : 'stream'})
-        const imageInBuffer = await convertStreamToBuffer(response.data);
-        return imageInBuffer;
-        } catch (error) {
-        console.log(error.message);
-    }
-}
+	try {
+		const response = await axios.get(url, {responseType : 'stream'});
+		const imageInBuffer = await convertStreamToBuffer(response.data);
+		return imageInBuffer;
+	} catch (error) {
+		console.log(error.message);
+	}
+};
 
 export const ResizeController = { resize };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import router from "./routes/resize.route.js";
+import express from 'express';
+import router from './routes/resize.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,10 +11,10 @@ const application = express();
 application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
 
-application.use('/resize', router)
+application.use('/resize', router);
 
 application.listen(port, () => {
-    console.log(`🚀 CloudCorp is running on port ${port}`);
+	console.log(`🚀 CloudCorp is running on port ${port}`);
 }); 
 
 export default application; 
